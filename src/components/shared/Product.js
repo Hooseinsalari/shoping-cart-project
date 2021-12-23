7d9706d478c64418490bcf771f6ff0b69265bb2c
Hooseinsalari <hossein.salari002@gmail.com>
2021-12-23 17:27:55 +0330

    set handlers

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 // styles
-import styles from "./Product.module.css"
+import styles from "./Product.module.css";
 
 // Functions
-import { shorten } from '../../helpers/functions';
+import { shorten, isInCart, quantityCount } from "../../helpers/functions";
+
+// context
+import { cartContext } from "../../context/CartContextProvider";
 
 const Product = ({ productData }) => {
-    return (
-        <div className={styles.cartContainer}>
-            <img src={productData.image} className={styles.img} alt='product' />
-            <div className={styles.information}>
-                <p>{shorten(productData.title)}</p>
-                <p>{productData.price}</p>
-            </div>
-            <div className={styles.cartFooter}>
-                <Link to={`/details/${productData.id}`}>Details</Link>
-                <button>add</button>
-            </div>
-        </div>
-    );
+  const { state, dispatch } = useContext(cartContext);
+
+  return (
+    <div className={styles.cartContainer}>
+      <img src={productData.image} className={styles.img} alt="product" />
+      <div className={styles.information}>
+        <p>{shorten(productData.title)}</p>
+        <p>{productData.price}</p>
+      </div>
+      <div className={styles.cartFooter}>
+        <Link to={`/details/${productData.id}`}>Details</Link>
+        {isInCart(state, productData.id) ? (
+          <button
+            onClick={() => dispatch({ type: "INCREASE", payload: productData })}
+          >
+            +
+          </button>
+        ) : (
+          <button
+            onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}
+          >
+            Add to cart
+          </button>
+        )}
+        {quantityCount(state, productData.id) === 1 && (
+          <button
+            onClick={() =>
+              dispatch({ type: "REMOVE_ITEM", payload: productData })
+            }
+          >
+            remove
+          </button>
+        )}
+        {quantityCount(state, productData.id) > 1 && (
+          <button
+            onClick={() => dispatch({ type: "DECREASE", payload: productData })}
+          >
+            -
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Product;
